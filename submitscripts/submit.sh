9581ebf1d6c1f7b1ecdcfb72795da9a945189317
irene bernardi <irene.bernardi.1@studenti.unipd.it>
2023-07-20 11:06:20 +0200

    1st SGE submit script

---
+++ submitscripts/submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#$ -N test
+#$ -cwd
+#$ -pe smp 30
+#$ -l h_vmem=128G
+
+# see the submission script
+cat $SGE_O_WORKDIR/submit.sh
+
+
+source /ddn/irenebernardi/.bashrc
+time mpiexec -n 4 nrniv -python -mpi CA3batch.py
